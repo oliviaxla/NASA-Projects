67a33c64f1e73144a84747daf939d7f26f375e49
oliviaxla <oliviajeon2007@gmail.com>
2026-08-03 19:34:30 -0700

    add code for morse-code project

---
+++ morse-code/project.ino
@@ -0,0 +1,159 @@
+ /*
+  SparkFun Inventor’s Kit
+  Circuit 1A-Blink
+
+  Turns an LED connected to pin 13 on and off. Repeats forever.
+
+  This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
+  This code is completely free for any use.
+
+  View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v41
+  Download code at: https://github.com/sparkfun/SIK-Guide-Code
+*/
+
+void setup() {
+
+  pinMode(13, OUTPUT);      // Set pin 13 to output
+
+}
+
+
+void loop() {
+
+  
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);   // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);   // Turn off the LED
+  delay(1500);              // Off for 1 second
+
+  
+
+  digitalWrite(13, HIGH);    // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);   // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(1500);              // On for 3 seconds
+  digitalWrite(13, LOW);    // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);    //Turn off the LED
+  delay(500);              //Off for 1 second
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);    //Turn off the LED
+  delay(1500);              //Off for 1 second
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(1500);              // On for 3 seconds
+  digitalWrite(13, LOW);    // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(1500);              // On for 3 seconds
+  digitalWrite(13, LOW);    // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(1500);              // On for 3 seconds
+  digitalWrite(13, LOW);    // Turn off the LED
+  delay(1500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);    // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);   // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);    // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);   // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);    // Turn on the LED
+  delay(500);              // On for 1 second
+  digitalWrite(13, LOW);   // Turn off the LED
+  delay(500);              // Off for 1 second
+
+  digitalWrite(13, HIGH);   //Turn on the LED
+  delay(1500);
+  digitalWrite(13, LOW);    //Turn off the LED
+  delay(1500);              //Off for 1.5 second
+
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  
+  
+  digitalWrite(13, HIGH);   //Turn on the LED
+  delay(1500);
+  digitalWrite(13, LOW);    //Turn off the LED
+  delay(500);              //Off for 1.5 second
+
+  digitalWrite(13, HIGH);   //Turn on the LED
+  delay(500);
+  digitalWrite(13, LOW);    //Turn off the LED
+  delay(500);              //Off for 1.5 second
+
+  digitalWrite(13, HIGH);   //Turn on the LED
+  delay(1500);
+  digitalWrite(13, LOW);    //Turn off the LED
+  delay(500);              //Off for 1.5 second
+
+  digitalWrite(13, HIGH);   //Turn on the LED
+  delay(1500);
+  digitalWrite(13, LOW);    //Turn off the LED
+  delay(1500);              //Off for 1.5 second
+
+  
+  digitalWrite(13, HIGH);
+  delay(1500);
+  
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+  
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+  
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(7500);
+
+
+
+
+}
+
